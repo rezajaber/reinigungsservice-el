1a1bf53a09e66725fcd5411dafc5b145915e3104
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-08 15:34:33 +0200

    import shadcn-packages

---
+++ src/components/ui/card/CardTitle.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from '@/lib/utils'
+
+const props = defineProps<{
+  class?: HTMLAttributes['class']
+}>()
+</script>
+
+<template>
+  <h3 :class="cn('', props.class)">
+    <slot />
+  </h3>
+</template>
