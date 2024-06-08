9fcb4aefaeb636e497e5fcc3ec3bd446ff2f7b5d
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-08 15:58:08 +0200

    organized paddings y-achse

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -15,20 +15,20 @@ import Footer from '../components/Footer.vue'
 <template>
   <div>
     <Header class="fixed inset-x-0 top-0 z-10 bg-white" />
-    <Hero class="" />
-    <AboutUs class="" />
+    <Hero class="py-20 lg:py-24" />
+    <AboutUs class="pb-20 lg:pb-24" />
 
-    <div class="bg-[#EBF3FE]">
+    <div class="grid gap-20 bg-[#EBF3FE] py-20 lg:py-24">
       <Vorteile class="" />
       <Einsatzorte class="" />
     </div>
 
-    <Leistungen class="" />
+    <Leistungen class="py-20 lg:py-24" />
 
-    <Einsatzgebiete class="" />
+    <Einsatzgebiete class="pb-20 lg:pb-24" />
     <!-- Additional services section with highlighted information -->
 
-    <ContactUs id="contact-us" class="x bg-primary" />
-    <Footer class="bg-popover-foreground" />
+    <ContactUs id="contact-us" class="bg-primary py-20 lg:py-24" />
+    <Footer class="bg-popover-foreground py-8 lg:py-12" />
   </div>
 </template>
