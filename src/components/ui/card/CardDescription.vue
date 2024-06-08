dec5c22f071dd192b55151667b051f489ca912ce
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-08 19:00:45 +0200

    change card-styling

--- src/components/ui/card/CardDescription.vue
+++ src/components/ui/card/CardDescription.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 </script>
 
 <template>
-  <p :class="cn('', props.class)">
+  <p :class="cn('text-sm text-muted-foreground', props.class)">
     <slot />
   </p>
 </template>
--- src/components/ui/card/CardTitle.vue
+++ src/components/ui/card/CardTitle.vue
@@ -8,7 +8,11 @@ const props = defineProps<{
 </script>
 
 <template>
-  <h3 :class="cn('', props.class)">
+  <h3
+    :class="
+      cn('text-2xl font-semibold leading-none tracking-tight', props.class)
+    "
+  >
     <slot />
   </h3>
 </template>
