6205c464549cfdc48efe1647542a63071b40a661
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-08 15:33:48 +0200

    Lesitungs-Section

--- src/components/Leistungen.vue
+++ src/components/Leistungen.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
-import CardComponent from '../components/ServiceCard.vue'
+import CardComponent from '@/components/ServiceCard.vue'
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel'
+import { Card, CardContent } from '@/components/ui/card'
 </script>
 
 <template>
   <div>
     <div class="mx-auto max-w-[1440px]">
-      <div class="">
+      <div class="mb-12">
         <span class="flex items-end gap-3 text-xl font-medium text-subtitle">
           <img class="mb-1.5 w-7" src="../assets/img/broom-icon.svg" alt="Broom Icon" />ABOUT
           COMPANY
@@ -17,12 +26,31 @@ import CardComponent from '../components/ServiceCard.vue'
         </h1>
       </div>
 
-      <CardComponent
-        image="../src/assets/img/service-card-one.jpeg"
-        title="Window Cleaning"
-        description="General Cleaning is a House a cleaning this includes dusting, floors, wiping."
-        link="/window-cleaning"
-      />
+      <div class="px-20">
+        <Carousel
+          class="relative w-full"
+          :opts="{
+            align: 'start'
+          }"
+        >
+          <CarouselContent>
+            <CarouselItem v-for="(_, index) in 5" :key="index" class="md:basis-1/2 lg:basis-1/3">
+              <div>
+                <Card class="py-2">
+                  <CardComponent
+                    image="../src/assets/img/service-card-one.jpeg"
+                    title="Window Cleaning"
+                    description="General Cleaning is a House a cleaning this includes dusting, floors, wiping."
+                    link="/window-cleaning"
+                  />
+                </Card>
+              </div>
+            </CarouselItem>
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </div>
     </div>
   </div>
 </template>
