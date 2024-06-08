7608d40aa065b9aec2a3384ad45f65d7560ef895
Reza Jaber <frontendrezajaber@gmail.com>
2024-06-08 15:15:16 +0200

    ServiceCard-Component und Leistung-Section

---
+++ src/components/Leistungen.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import CardComponent from '../components/ServiceCard.vue'
+</script>
+
+<template>
+  <div>
+    <div class="mx-auto max-w-[1440px]">
+      <div class="">
+        <span class="flex items-end gap-3 text-xl font-medium text-subtitle">
+          <img class="mb-1.5 w-7" src="../assets/img/broom-icon.svg" alt="Broom Icon" />ABOUT
+          COMPANY
+        </span>
+
+        <h1 class="mt-6 text-5xl font-bold">
+          Our Promise Is To Deliver Only <br />The Best
+          <span class="font-title text-primary">Services</span>
+        </h1>
+      </div>
+
+      <CardComponent
+        image="../src/assets/img/service-card-one.jpeg"
+        title="Window Cleaning"
+        description="General Cleaning is a House a cleaning this includes dusting, floors, wiping."
+        link="/window-cleaning"
+      />
+    </div>
+  </div>
+</template>
---
+++ src/components/ServiceCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+import Button from './ui/button/Button.vue'
+import { ArrowUpRight, Warehouse } from 'lucide-vue-next'
+
+defineProps({
+  image: String,
+  title: String,
+  description: String,
+  link: String
+})
+</script>
+
+<template>
+  <div class="w-[410px] overflow-hidden rounded-xl pb-4 shadow-lg">
+    <img class="w-full object-cover" :src="image" alt="Card image" />
+
+    <div class="relative grid place-items-center px-6 py-4 text-center">
+      <div class="rhombus absolute -top-11 flex w-56 justify-center bg-white py-6">
+        <Warehouse class="h-10 w-10 stroke-1" />
+      </div>
+
+      <h3 class="mt-8 text-2xl font-bold">{{ title }}</h3>
+      <p class="text-description mt-3.5 font-light leading-7">
+        {{ description }}
+      </p>
+      <Button class="mt-6 h-11 px-8">See More<ArrowUpRight class="ml-1.5 h-5 w-5" /></Button>
+    </div>
+  </div>
+</template>
+
+<style>
+.rhombus {
+  clip-path: polygon(50% 0, 100% 50%, 50% 100%, 0% 50%);
+}
+</style>
